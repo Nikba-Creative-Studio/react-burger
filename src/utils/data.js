0b99506a36b4ac63c9cfd68c2c76bfd23d6fd781
Bargan Nicolai <41567806+Nikba-Creative-Studio@users.noreply.github.com>
2022-01-19 17:03:04 +0200

    BurgerConstructor компонент

--- src/utils/data.js
+++ src/utils/data.js
@@ -257,6 +257,48 @@ export const data =
       "__v":0
    },
    {
+      "_id":"60666c42cc7b410027a1a9bf",
+      "name":"Сыр с астероидной плесенью",
+      "type":"main",
+      "proteins":84,
+      "fat":48,
+      "carbohydrates":420,
+      "calories":3377,
+      "price":4142,
+      "image":"https://code.s3.yandex.net/react/code/cheese.png",
+      "image_mobile":"https://code.s3.yandex.net/react/code/cheese-mobile.png",
+      "image_large":"https://code.s3.yandex.net/react/code/cheese-large.png",
+      "__v":0
+   },
+   {
+      "_id":"60666c42cc7b410027a1a9b3",
+      "name":"Филе Люминесцентного тетраодонтимформа",
+      "type":"main",
+      "proteins":44,
+      "fat":26,
+      "carbohydrates":85,
+      "calories":643,
+      "price":988,
+      "image":"https://code.s3.yandex.net/react/code/meat-03.png",
+      "image_mobile":"https://code.s3.yandex.net/react/code/meat-03-mobile.png",
+      "image_large":"https://code.s3.yandex.net/react/code/meat-03-large.png",
+      "__v":0
+   },
+   {
+      "_id":"60666c42cc7b410027a1a9b6",
+      "name":"Биокотлета из марсианской Магнолии",
+      "type":"main",
+      "proteins":420,
+      "fat":142,
+      "carbohydrates":242,
+      "calories":4242,
+      "price":424,
+      "image":"https://code.s3.yandex.net/react/code/meat-01.png",
+      "image_mobile":"https://code.s3.yandex.net/react/code/meat-01-mobile.png",
+      "image_large":"https://code.s3.yandex.net/react/code/meat-01-large.png",
+      "__v":0
+   },
+   {
       "_id":"60666c42cc7b410027a1a9be",
       "name":"Мини-салат Экзо-Плантаго",
       "type":"main",
